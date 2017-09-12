05d2239ee70b15044dac72ce64f1f95471b1252c
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-12 16:20:46 +0530

    added my-account view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   get 'logout',  to: 'sessions#destroy'
+  get 'my_account', to: 'my_account#index'
+  post 'stripe_events', to: 'stripe#events', as: 'stripe_events'
 
   resources :users
   resources :sessions
