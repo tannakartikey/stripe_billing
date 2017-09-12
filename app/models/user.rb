209c3bb61515bfa87dec7521b9097e66b0ca07e9
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-12 16:17:36 +0530

    Creating Stripe subscription of a user if any plan other than free is selected StripeCustomer will be created if stripe_customer_id is nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_uniqueness_of :email
+
+  belongs_to :plan
+
+  after_create :create_stripe_subscription
+
+  def create_stripe_subscription
+    unless self.plan.name == 'Free'
+      StripeSubscription.create(self, self.plan.stripe_id, (Date.today + 14).to_time.to_i)
+    end
+  end
 end
