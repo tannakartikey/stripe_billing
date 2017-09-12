ec0576b0ef540bd81a4c9dfd2d4f86567f023f28
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-12 16:21:57 +0530

    redirecting user to my_account_path after signup/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in"
+      redirect_to my_account_path, notice: "Logged in"
     else
       flash.now.alert = "Email of password wrong"
       render "new"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signup"
+      redirect_to my_account_path, notice: "Thank you for signup"
     else
       render "new"
     end
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :plan_id)
   end
 end
