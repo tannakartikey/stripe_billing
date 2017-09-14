a2071eecedda48f1684d7cad3410a8591909b352
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-14 13:24:34 +0530

    Added Add Credit Card functionality

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,41 @@
+class CardsController < ApplicationController
+  def new
+  end
+
+  def create
+    begin
+      @customer = StripeCustomer.retrieve(current_user)
+      @source = @customer.sources.create({:source => params[:stripeSource]})
+      @customer.default_source = @source.id
+      @customer.save
+      current_user.payment_source = @source.id
+      current_user.save!
+    rescue Stripe::CardError => error
+      flash[:error] = error.message
+      redirect_to new_card_path and return
+    end
+
+    if current_user.subscription_id == nil
+      begin
+        StripeSubscription.create(current_user)
+      rescue Stripe::CardError => error
+        flash[:error] = "There is some problem with your card. Please consult your bank"
+        redirect_to new_card_path and return
+      end
+    end
+
+    respond_to do |format|
+      format.html { redirect_to my_account_path, notice: notice }
+    end
+  end
+
+  private 
+
+  def notice
+    notice = 'Card was successfully updated.'
+    if current_user.subscription_id.nil?
+      notice += " You are successfully subscribed. Cheers!"
+    end
+    notice
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
+  resource :card
 
 end
