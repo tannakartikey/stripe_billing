3221fd6ce503e538c5b33f1391a7b3a4c3f50fb5
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-14 16:48:02 +0530

    Implemented subscription cancellation

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -3,6 +3,10 @@ class MyAccountController < ApplicationController
   before_filter :authorize, only: [:pro]
   def index
   end
-  def pro
+  def destroy
+    StripeSubscription.delete(current_user)
+    respond_to do |format|
+      format.html { redirect_to my_account_url, alert: 'Subscription was cancelled successfully' }
+    end
   end
 end
