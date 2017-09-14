ebdfaa0b05aac16d414fe1bd21b1cb6a603df1cf
Kartikey Tanna <tannakartikey@gmail.com>
2017-09-14 13:23:13 +0530

    displaying plan infromation on My Account page

---
+++ app/helpers/my_account_helper.rb
@@ -0,0 +1,18 @@
+module MyAccountHelper
+  def promotion_message_if_free_plan_users
+    benefits = "<br>
+    <ul>
+      <li>Benefit 1</li>
+      <li>Benefit 2</li>
+    </ul>
+    </br>"
+    "<br>Subscribe to our paid plan avail #{benefits}".html_safe
+  end
+
+  def if_no_payment_source_for_pro_user
+    if current_user.payment_source.nil? && !current_user.plan.stripe_id.nil?
+    "Enter your card details to continue using subscription<br>
+     after trial end. You will be charged only after trial period".html_safe
+    end
+  end
+end
